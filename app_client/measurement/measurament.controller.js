825e3583581987d7ef9d2f38c768ca52bdd52175
Josip Punis <josippunis@gmail.com>
2017-01-27 12:18:12 +0100

    Dodan refresh i filter
    Dodan refresh i filter u mjerenjima

--- app_client/measurement/measurament.controller.js
+++ app_client/measurement/measurament.controller.js
@@ -1,13 +1,20 @@
 /**
  * Created by josip on 26.1.2017..
  */
-angular.module('mainModule').controller('measurementCtrl', function ($location,measurementData){
+angular.module('mainModule').controller('measurementCtrl', function ($location,$route,measurementData){
     var vm=this
     vm.msg
     vm.err;
     vm.succ;
     vm.all_meas=[]
     vm.new_meas={}
+    
+    vm.search={
+        bodypart:"",
+        created:"",
+        unit:"",
+        value:""
+    }
     
     measurementData.getAllMeasurement().then(function (res){
         vm.all_meas=res.data
@@ -21,6 +28,7 @@ angular.module('mainModule').controller('measurementCtrl', function ($location,m
                 vm.succ=true;
                 vm.err=false;
                 vm.msg="Novi zapis je uspješno zabilježen u bazu podataka"
+                $route.reload();
             }
         }).catch(function(res){
             vm.err=true;
@@ -36,6 +44,7 @@ angular.module('mainModule').controller('measurementCtrl', function ($location,m
             vm.succ=true;
             vm.err=false;
             vm.msg="Zapis je uspješno izbrisan"
+            $route.reload();
         })
     }
 
